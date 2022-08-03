29a52856c327a900207902a549597cd678ecee99
robert-virtual <robertocastill63@gmail.com>
2022-08-02 20:37:43 -0600

    using rest api

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,43 @@
 <script lang="ts">
+  import { sheetsApi } from "./axios/sheetsApi";
+
   let gclient;
   console.log({ google });
   let { spreadsheetId, access_token } = localStorage;
-  let gapiLoaded = false;
-  async function initClient() {
-    try {
-      await gapi.client.init({
-        apiKey: "AIzaSyCSn8kzs78gr1npZD-J-VwSbiyRp_KupCs",
-        clientId:
-          "1047629314149-f5sp9o7ol8ta174r41hur8untntt188j.apps.googleusercontent.com",
-        discoveryDocs: [
-          "https://sheets.googleapis.com/$discovery/rest?version=v4",
-        ],
-        scope: "https://www.googleapis.com/auth/spreadsheet",
-      });
-    } catch (error) {
-      console.warn(error);
-    }
-    gapiLoaded = true;
-    console.log("gapi.client loaded", gapi.client);
-
-    if (!access_token) {
-      console.log("not access token available");
-      return;
-    }
-    gapi.client.setToken({ access_token });
-    console.log("sheetId: ", spreadsheetId);
-    if (!spreadsheetId) {
-      await createSheet();
-    }
-  }
-  $: if (gapiLoaded) {
-    console.log("gapi loaded");
+  console.log({ spreadsheetId, access_token });
+  if (!spreadsheetId) {
+    createSheet();
   }
   let notes = [];
-  gapi.load("client", initClient);
   async function loadNotes() {
-    console.log("gapi.client", gapi.client);
-    const res = await gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId,
-      range: "Hoja 1!A:B",
-    });
-    notes = res.result.values;
+    const range = "Hoja 1!A:B";
+    const res = await sheetsApi.get(`/${spreadsheetId}/values/${range}`);
+    console.log({ res });
   }
   let title = "";
   async function createSheet() {
-    if (!gapi.client) {
-      console.log("gapi.client not initialized");
-      return;
-    }
-    const newSheet = await gapi.client.sheets.spreadsheets.create({
+    const { data } = await sheetsApi.post("/", {
       properties: {
         title: "notes-app-db",
       },
     });
-    localStorage.spreadsheetId = newSheet.result.spreadsheetId;
+    localStorage.spreadsheetId = data.spreadsheetId;
     spreadsheetId = localStorage.spreadsheetId;
 
-    console.log("new sheet creaed: ",{ newSheet });
-    const res = await gapi.client.sheets.spreadsheets.values.update({
-      spreadsheetId,
-      range: "Hoja 1!A1:B1",
-      resource: {
+    console.log("new sheet creaed: ", { data });
+
+    const range = "Hoja 1!A1:B1";
+    const res = await sheetsApi.put(
+      `/${spreadsheetId}/values/${range}`,
+      {
         values: [["title", "content"]],
       },
-    });
+      {
+        params: {
+          valueInputOption: "RAW",
+        },
+      }
+    );
   }
   function oauth2Login() {
     gclient = google.accounts.oauth2.initTokenClient({
@@ -73,6 +46,10 @@
       scope: "https://www.googleapis.com/auth/spreadsheets",
       callback: (token) => {
         localStorage.access_token = token.access_token;
+        access_token = localStorage.access_token;
+        sheetsApi.defaults.params = {
+          access_token,
+        };
       },
     });
     gclient.requestAccessToken();
